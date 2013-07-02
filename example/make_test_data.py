1348378d8959e32ba0bcb01bc0f56a46be174077
xiechao06 <xiechao06@gmail.com>
2013-07-02 16:59:08 +0800

    fix #3

--- example/make_test_data.py
+++ example/make_test_data.py
@@ -2,8 +2,6 @@
 # -*- coding: UTF-8 -*-
 
 from basemain import db
-from flask.ext.report import models as builtin_models
-builtin_models.setup_models(db)
 import models
 db.create_all()
 
@@ -27,8 +25,3 @@ ZhangFei = commit(models.User(name=u'张飞', strength=90, intelligience=50, imp
 SunQuan = commit(models.User(name=u"孙权", strength=65, intelligience=70, impact=80, group=Wu, is_lord=True))
 ZhouYu = commit(models.User(name=u'周瑜', strength=60, intelligience=90, impact=60, group=Wu))
 LuSu = commit(models.User(name=u'鲁肃', strength=10, intelligience=85, impact=50, group=Wu))
-
-data_set1 = commit(builtin_models.DataSet(name="dataset 1"))
-report1 = commit(builtin_models.Report(name="report 1", creator="James T. Kirk", data_set=data_set1))
-filter_cond1 = commit(builtin_models.FilterCond(name="User.is_lord", value=False, report=report1))
-order_by1 = commit(builtin_models.OrderBy(name=u"平均影响力", desc=True, report=report1))
---
+++ flask_report/data_set.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+import os
+import yaml
+from import_file import import_file
+from werkzeug.utils import cached_property
+
+class DataSet(object):
+
+    def __init__(self, report_view, id_):
+        self.report_view = report_view
+        self.id_ = id_
+        data_set_meta_file = os.path.join(self.report_view.data_set_dir, str(id_), 'meta.yaml')
+        data_set_meta = yaml.load(file(data_set_meta_file).read())
+        self.name = data_set_meta['name'] 
+        self.creator = data_set_meta.get('creator')
+        self.create_time = data_set_meta.get('create_time')
+
+    @cached_property
+    def query(self):
+        query_def_file = os.path.join(self.report_view.data_set_dir, str(self.id_), "query_def.py")
+        lib = import_file(query_def_file)
+        return lib.get_query(self.report_view.db, self.report_view.model_map)
+
+    @cached_property
+    def columns(self):
+        def _make_dict(idx, c):
+            if hasattr(c['expr'], '_element'): # is label
+                name = c['name'] or dict(name=str(c['expr']))
+                key = c['expr']._element.name
+            else:
+                name = str(c['expr'])
+                key = name
+            
+            return dict(idx=idx, name=name, key=key, expr=c['expr'])
+
+        return tuple(_make_dict(idx, c) for idx, c in enumerate(self.query.column_descriptions))
+
